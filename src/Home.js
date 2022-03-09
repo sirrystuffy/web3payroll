a8eb6d910be954119d8463b8436d604acac9c325
Gordon Fung <northeman55@gmail.com>
2022-03-09 03:02:09 -0500

    Trying something out

---
+++ src/Home.js
@@ -0,0 +1,55 @@
+
+function Home() {
+
+    return (
+        <div className='HomeClass'> 
+        <h1 className='NavBar'>Home Account Schedule Timecards Employers</h1> 
+        
+        <h2 className= 'HomePageSchedule'
+        style=
+        {
+        {width: 700, height: 500}
+        }>
+            This Week's Schedule
+        </h2>
+
+        <h2 className='helpPanel'
+        style=
+        {
+            {width: 200, height: 500}
+        }
+        >
+            Help and Services
+        </h2>
+
+        <h2 className='quicklinksPanel'
+        style=
+        {
+            {width: 200, height: 250}
+        }
+        >
+           Quick Links 
+        </h2>
+
+        <h2 className='PaymentPanel'
+        style=
+        {
+            {width: 200, height: 250}
+        }
+        >
+           Payments 
+        </h2>
+        
+
+           
+        
+        </div>
+
+        
+
+    )
+}
+
+
+
+export default Home
