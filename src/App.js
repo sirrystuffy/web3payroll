bd947503f954e9b8612304faabedf86defa66273
Gordon Fung <gfung@albany.edu>
2022-03-09 08:26:42 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Home from "./Home";
+import initialize from "./start/contract"
+import './App.css'
 function App() {
+  
   return (
     <div>
-     <Home/>
-     
+      <Home />
+      <section>
+        <button id="connectButton" onClick={initialize} disabled >Connect MetaMask</button>
+      </section>
     </div>
   );
+  
 }
 
 export default App;
