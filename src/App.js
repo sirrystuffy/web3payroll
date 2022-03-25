a4d804b0a6e22ce9abbf855c4993a450041acb98
Gordon Fung <gfung@albany.edu>
2022-03-25 23:06:07 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, { useState, useEffect } from "react";
 import Home from "./Home";
-import initialize from "./start/contract"
-import './App.css'
+import initialize from "./metamask/contract";
+import axios from "axios"
+import './App.css';
+
 function App() {
+  const[text, setText] = useState("");
+  
+  useEffect(() => {
+    axios.get("http://localhost:5000/backend").then(response => {
+      setText(response.data)
+    })
+  }, []) //Pass in an empty array to ensure it only returns once. 
+
+
   
   return (
-    <div>
+    <div className="App">
       <Home/>
       <section>
         <button id="connectButton" onClick={initialize} disabled >Connect MetaMask</button>
       </section>
+      {text}
     </div>
   );
   
