f5471177fb5e47634fb65b3c7bc8b061b3d2045a
Izaaz Izzadeen <imizzadeen@albany.edu>
2022-04-17 20:15:10 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
 import Home from "./Home";
-import initialize from "./metamask/contract";
-import axios from "axios"
-import './App.css';
-
+import Account from "./Account";
+import Timecards from "./Timecards";
+import Employees from "./Employees";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
-  const[text, setText] = useState("");
-  
-  useEffect(() => {
-    axios.get("http://localhost:5000/backend").then(response => {
-      setText(response.data)
-    })
-  }, []) //Pass in an empty array to ensure it only returns once. 
-
-
-  
   return (
-    <div className="App">
-      <Home/>
-      <section>
-        <button id="connectButton" onClick={initialize} disabled >Connect MetaMask</button>
-      </section>
-      {text}
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="account" element={<Account />} />
+      <Route path="timecards" element={<Timecards />} />
+    </Routes>
+  </BrowserRouter>
+
   );
-  
 }
 
 export default App;
