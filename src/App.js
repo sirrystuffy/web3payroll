1819fc6e515c6c64e5e9e69941751637d13e74bf
Gordon Fung <gfung@albany.edu>
2022-04-26 21:37:22 +0000

    Re-added metamask button and useEffect and useState

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Account from "./Account";
 import Timecards from "./Timecards";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
 } from "react-router-dom";
+
 function App() {
+  const[text, setText] = useState("");
+  
+  useEffect(() => {
+    axios.get("http://localhost:5000/employees").then(response => {
+      const workers = response.data.employees;
+      // console.log(workers[0].email);
+      setText(workers[0].email);
+    })
+  }, []) //Pass in an empty array to ensure it only returns once. 
+
   return (
     <BrowserRouter>
     <Routes>
@@ -17,6 +29,10 @@ function App() {
       <Route path="account" element={<Account />} />
       <Route path="timecards" element={<Timecards />} />
     </Routes>
+    <section>
+        <button id="connectButton" onClick={initialize} disabled>Connect MetaMask</button>
+    </section>
+    {text}
   </BrowserRouter>
 
   );
