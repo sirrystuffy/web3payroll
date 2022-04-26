8a6dfeb4d22d7e79ce0b6822c0e44fb116478c11
Gordon Fung <gfung@albany.edu>
2022-04-26 21:31:17 +0000

    Made employees endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 var cors = require("cors")
 const port = 5000; 
+
 app.use(cors()); //Install cors()
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 require('dotenv').config();
-var db = require('web3.storage') //requrie(package).property
+
+var db = require('web3.storage'); //requrie(package).property
+
+let empjson = require('/Users/powerpony/Downloads/CSI499/backend/files/employee.json');
 
 function getAccessToken () {
     // If you're just testing, you can paste in a token
@@ -22,11 +30,21 @@ function getAccessToken () {
 // Create a client instance to begin uploading/retrieving files
 const client = new db.Web3Storage({token: getAccessToken()});
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", async (req, res) => res.send("Hello World"));
 
 // When someone sends a GET request to the specified route, we send a response 
 app.get("/backend", async (req, res) => {
     res.send("Backend is working")
+});
+
+app.get("/employees", async (req, res) => {
+    // const empFile = client.status("bafybeie74n6xxjeymhvqgybros4f5goz2mtevnbk4vahpokxx5hdm4xtzq");
+    const empFile = await client.get("bafybeie74n6xxjeymhvqgybros4f5goz2mtevnbk4vahpokxx5hdm4xtzq");
+    // console.log((await empFile).json())
+    // res.send((await empFile));
+    res.json(empjson);
+    
+    // res.json((await ( empFile.text())).substring(190, 886));
 });
 
 console.log(process.env.APIKEY); //environmnet var set through local machine. 'export APIKEY=SOMETHING'
